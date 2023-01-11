47afc9ae43f47f2ab9cac2498e9b9dda5bf8eeaa
tpemba100 <47162311+tpemba100@users.noreply.github.com>
2023-01-11 15:49:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,1 @@
-import './App.css';
-import { NavBar } from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Projects from './components/Projects';
-import Intro from './components/Intro';
-import About from './components/About';
 
-function App() {
-  return (
-    <div className='App'>
-      <NavBar />
-      <Intro />
-      <About />
-      <Projects />
-    </div>
-  );
-}
-
-export default App;
